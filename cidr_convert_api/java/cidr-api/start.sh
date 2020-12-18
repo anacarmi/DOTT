f653656d63b1b3db2ab7892ca0b8d415a143a3c1
anacarmi <anacarminamg@gmail.com>
2020-12-17 19:24:37 -0800

    Update start.sh

--- cidr_convert_api/java/cidr-api/start.sh
+++ cidr_convert_api/java/cidr-api/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#JENKINS_NODE_COOKIE=dontKillMe
-nohup java -cp target/cidr-api-1.0-SNAPSHOT-jar-with-dependencies.jar com.dott.App >/dev/null 2>&1 &
+JENKINS_NODE_COOKIE=dontKillMe
+nohup java -cp target/cidr-api-1.0-SNAPSHOT-jar-with-dependencies.jar com.dott.App &  #>/dev/null 2>&1
