2ab4cdb937ffd43488889d8a2ac6ab82a30b2e28
anacarmi <anacarminamg@gmail.com>
2020-12-17 19:02:35 -0800

    Update start.sh

--- cidr_convert_api/java/cidr-api/start.sh
+++ cidr_convert_api/java/cidr-api/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup java -cp target/cidr-api-1.0-SNAPSHOT-jar-with-dependencies.jar com.dott.App 2>/dev/null &
+nohup java -cp target/cidr-api-1.0-SNAPSHOT-jar-with-dependencies.jar com.dott.App >/dev/null 2>&1 &
